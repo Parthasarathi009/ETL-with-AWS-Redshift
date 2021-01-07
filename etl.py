891e5803151ed8054e4d8d5d5de046a092da350b
Parthasarathi Samantaray <parthasarathi.samantaray87@gmail.com>
2021-01-07 05:58:45 -0500

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,40 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    """Loads data from json logs into stagging tables"""
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    """Insert data from stagging tables to fact and dimension tables"""
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    -Reads the config 
+    -Connects to Postgress Database
+    -Calls function to Loads data from json logs into stagging tables
+    -Calls function to Insert data from stagging tables to fact and dimension tables
+    """
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
